e1b7227e8c47a12ea0f148617b96b24585347ca1
ws0671 <ws0671@naver.com>
2023-05-02 14:59:57 +0900

    Feat: mdx 연결하기

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,7 +8,19 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss"],
+  plugins: [
+    `gatsby-plugin-mdx`,
+    "gatsby-plugin-postcss",
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/blog-posts`,
+      },
+    },
+  ],
 };
 
 export default config;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
 export default function IndexPage() {
   return (
@@ -8,12 +9,30 @@ export default function IndexPage() {
           <span>Articles</span> <span>About</span>
         </div>
         <div className="flex">
-          <div className="mr-2 h-10 w-10 rounded-full bg-sky-300"></div>
+          <div className="mr-2 h-10 w-10">
+            <StaticImage
+              className="rounded-full"
+              src="../images/sheep_ttoja.png"
+              alt="profile"
+            />
+          </div>
           <span>Lee young min</span>
         </div>
-        <div>
-          <span>깃허브</span>
-          <span>메일</span>
+        <div className="flex items-center">
+          <div>
+            <StaticImage
+              height={40}
+              src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+              alt="Github logo"
+            />
+          </div>
+          <div>
+            <StaticImage
+              height={30}
+              src="https://cdn-icons-png.flaticon.com/512/666/666162.png"
+              alt="mail logo"
+            ></StaticImage>
+          </div>
         </div>
       </div>
     </div>
